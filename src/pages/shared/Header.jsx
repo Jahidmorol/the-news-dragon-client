b6c4612af50b7112cbf76442d6b38fb0b970c8af
jahid morol <jahidmorol2@gmail.com>
2023-04-27 13:50:17 +0600

    added header section with responsive for all device

--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
-import moment from 'moment';
-import logo from '../../assets/logo.png'
+import React from "react";
+import moment from "moment";
+import logo from "../../assets/logo.png";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
-    return (
-        <div className='text-center'>
-            <img src={logo} alt="" />
-            <p className='my-2 '>Journalism Without Fear or Favour</p>
-            <h5>{moment().format("dddd, MMMM Do YYYY")}</h5>
-        </div>
-    );
+  return (
+    <Container>
+      <div className="text-center">
+        <img src={logo} alt="photo" />
+        <p className="my-2 ">Journalism Without Fear or Favour</p>
+        <h5>{moment().format("dddd, MMMM Do YYYY")}</h5>
+      </div>
+      <div className="d-flex">
+        <Button variant="danger">Latest</Button>
+        <Marquee className="text-danger" speed={60}>
+          Match Highlights: Germany vs Spain — as it happened ! Match
+          Highlights: Germany vs Spain as... Match Highlights: Germany vs Spain
+          — as it happened ! Match Highlights: Germany vs Spain as...
+        </Marquee>
+      </div>
+      <Navbar collapseOnSelect expand="lg" bg="" variant="" className="my-3">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+              <Nav.Link href="#pricing">Career</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Profile</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                <Button variant="dark">Login</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
+  );
 };
 
 export default Header;
