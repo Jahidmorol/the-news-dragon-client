09d2f0b750c645b043e6e6a67d211ab0a8d3780f
jahid morol <jahidmorol2@gmail.com>
2023-04-30 05:14:53 +0600

    added readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+ The News Dragon Clint
+ Live link <https://the-news-dragon-client-68fd4.web.app>
+ Server link <https://the-news-dragon-server-jahidmorol.vercel.app/news>
--- src/pages/shared/LeftNav/LeftNav.jsx
+++ src/pages/shared/LeftNav/LeftNav.jsx
@@ -1,30 +1,35 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        fetch('https://the-news-dragon-server-jahidmorol.vercel.app/categories')
-        .then(res => res.json())
-        .then(data => setCategories(data))
-        .catch(error => console.error(error))
-    } ,[])
+  useEffect(() => {
+    fetch("https://the-news-dragon-server-jahidmorol.vercel.app/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.error(error));
+  }, []);
 
-    return (
-        <div>
-            <h4>All Caterogy</h4>
-            <div className='my-3'>
-                {
-                    categories.map(category => <p key={category.id}>
-                        <Link to={`/category/${category.id}`} className="text-decoration-none text-black">{category.name}</Link>
-                    </p>)
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h4>All Caterogy</h4>
+      <div className="my-3">
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link
+              to={`/category/${category.id}`}
+              className="text-decoration-none text-black"
+            >
+              {category.name}
+            </Link>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeftNav;
